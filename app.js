60af5146b981e4a9c1ccb4d4b366114ce07fe414
PCLongmont <PCLongmont@gmail.com>
2014-08-14 12:19:36 -0600

    more scaffolding, included passport google and local, linked client.js to layout

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
+
+// ______________________________________________________________________
 var express = require('express');
 var bodyParser = require('body-parser');
+
+
+// ______________________________________________________________________
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+		passport.serializeUser(function(user, done) {
+		  done(null, user.id);
+		});
+
+		passport.deserializeUser(function(id, done) {
+		  User.findById(id, function(err, user) {
+		    done(err, user);
+		  });
+		});
+
+
+var passport = require('passport')
+	, GoogleStrategy = require('passport-google').Strategy;
+
+
+
+
+// ______________________________________________________________________
 
 var app = express();
 app.set('view engine', 'jade');
@@ -11,6 +52,56 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+
+
+// _____________________________________________________________________
+
+// // Redirect the user to Google for authentication.  When complete, Google
+// // will redirect the user back to the application at
+// //     /auth/google/return
+// app.get('/auth/google', passport.authenticate('google'));
+
+// // Google will redirect the user to this URL after authentication.  Finish
+// // the process by verifying the assertion.  If valid, the user will be
+// // logged in.  Otherwise, authentication has failed.
+// app.get('/auth/google/return', 
+//   passport.authenticate('google', { successRedirect: '/',
+//                                     failureRedirect: '/login' }));
+
+// passport.use(new GoogleStrategy({
+// 	returnURL: 'http://www.example.com/auth/google/return',
+// 	realm: 'http://www.example.com/'
+// 	},
+// 	function(identifier, profile done) {
+// 		User.findOrCreate({ openId: identifier }, function(err, user) {
+// 			done(err, user);
+// 		});
+// 	}
+// ));
+
+
+// ______________________________________________________________________
 var server = app.listen(3705, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/scripts/client.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+
+	$('body').append('<h1>What the hell!</h1>');
+
+
+
+
+
+})
